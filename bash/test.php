56b2f5989d958f33d049d43c3b97b66a853a7bea
Michael Seidel <github@seidel-michael.de>
2016-09-08 23:01:21 +0200

    Created Newsletter DatabaseModel

--- bash/test.php
+++ bash/test.php
@@ -1,16 +1,3 @@
 <?php
-
-
-require_once(__DIR__.'/../private/dataManager/newsletter/NewsletterDataManager.php');
-
-use dataManager\newsletter\NewsletterDataManager;
-
-for($i = 0; i < 100; $i++)
-{
-    echo "Test ($i)";
-} 
- 
-//phpinfo();
-
-
+echo "Test";
 ?>
